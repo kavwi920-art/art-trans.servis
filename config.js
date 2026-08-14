f6e5ea063f1b4b2fe76d78e5b2ef7ccb0ce3ab73
kavwi920-art <kavwi920@gmail.com>
2026-08-14 14:45:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = 'https://akofqqpoknpwhqhonnst.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrb2ZxcXBva25wd2hxaG9ubnN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2MTQ3MDcsImV4cCI6MjEwMjE5MDcwN30.NZWcqIU5U4COT39T71Ft9nUW_6BJUcNugNZmbOVaIf4';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Tworzymy klienta pod inną nazwą (supabase jest już zajęte przez bibliotekę)
+window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
